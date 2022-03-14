e0434ad03c749df1c43168b18a11d6efeb2cc088
yang <952711716@qq.com>
2022-03-14 17:42:27 +0800

    build: 添加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindCSS from 'vite-plugin-windicss'
+// @ts-ignore
+import path from 'path'
+
+const pathResolve = (pathStr: string) => {
+  return path.resolve(__dirname, pathStr)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    WindCSS(),
+  ],
+  alias: {
+    '@/': pathResolve('./'),
+  },
+  resolve: {
+    alias: [
+      { find: /^~/, replacement: '' },
+    ],
+  },
 })
